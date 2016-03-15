c060e62257726a226639de9727781a5cc12a70e2
Rodman <denisrodman1980@gmail.com>
2016-03-15 22:50:47 +0300

    Add script to install Drupal code standards.

---
+++ examples/scripts/post-executed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo chown vagrant:vagrant /var/www/
+sudo apt-get install php_codesniffer
+
+PHPCS='/usr/share/php/PHP/CodeSniffer/Standards/'
+VERSION_CODER='coder-7.x-2.5.tar.gz'
+CODER="https://ftp.drupal.org/files/projects/$VERSION_CODER"
+
+if [ -d "$PHPCS" ]; then
+  echo "`pwd`"
+  echo 'We have to install Drupal code standards'
+  cd '/home/vagrant'
+  wget "$CODER"
+  tar -xvf "$VERSION_CODER"
+  sudo cp -R 'coder/coder_sniffer/Drupal' $PHPCS
+else
+  echo 'Drupal code standards are already installed'
+  echo 'Continue...'
+fi
