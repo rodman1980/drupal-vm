096e0a369e497850c19e95ed1d246af77ee4a662
Rodman <denisrodman1980@gmail.com>
2016-03-15 23:37:17 +0300

    Do some cleanup of the code.

--- examples/scripts/post-executed.sh
+++ examples/scripts/post-executed.sh
@@ -2,17 +2,15 @@
 
 sudo pear install PHP_CodeSniffer
 
-PHPCS='/usr/share/php/PHP/CodeSniffer/Standards/'
-VERSION_CODER='coder-7.x-2.5.tar.gz'
-CODER="https://ftp.drupal.org/files/projects/$VERSION_CODER"
+PHPCS=/usr/share/php/PHP/CodeSniffer/Standards/
+VERSION_CODER=coder-7.x-2.5.tar.gz
+CODER=https://ftp.drupal.org/files/projects/$VERSION_CODER
 
 if [ -d "$PHPCS" ]; then
   echo "`pwd`"
   echo 'We have to install Drupal code standards'
   cd '/home/vagrant'
-  wget "$CODER"
-  tar -xvf "$VERSION_CODER"
-  sudo cp -R 'coder/coder_sniffer/Drupal' $PHPCS
+  wget "$CODER" | tar -xvf "$VERSION_CODER" | sudo cp -R 'coder/coder_sniffer/Drupal' $PHPCS
 else
   echo 'Drupal code standards are already installed'
   echo 'Continue...'
