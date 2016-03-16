295fba960d310e3a5796b02af4258630b0ad3a96
Haurylovich, Dzianis <Dzianis_Haurylovich@epam.com>
2016-03-16 10:42:56 +0300

    Change an error handling in the script.

--- examples/scripts/post-executed.sh
+++ examples/scripts/post-executed.sh
@@ -6,11 +6,17 @@ PHPCS=/usr/share/php/PHP/CodeSniffer/Standards/
 VERSION_CODER=coder-7.x-2.5.tar.gz
 CODER=https://ftp.drupal.org/files/projects/$VERSION_CODER
 
-if [ -d "$PHPCS" ]; then
+if [ ! -d "$PHPCS/Drupal" ]; then
   echo `pwd`
-  echo 'We have to install Drupal code standards'
   cd '/home/vagrant'
-  wget "$CODER" | tar -xvf "$VERSION_CODER" | sudo cp -R 'coder/coder_sniffer/Drupal' $PHPCS
+
+  echo 'We have to install Drupal code standards'
+  if [ -d coder ]; then
+      echo 'Found coder folder. Delete coder folder.'
+      rm coder -R
+  fi
+
+  wget "$CODER" && tar -xvf "$VERSION_CODER" &> /dev/null && sudo cp -R 'coder/coder_sniffer/Drupal' $PHPCS && echo 'Drupal code standards have been successfully copied'
 else
   echo 'Drupal code standards are already installed'
   echo 'Continue...'
