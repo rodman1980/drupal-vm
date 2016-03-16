156b1a80c45a8eae46b00838c6b1d8e7583fdd4b
Haurylovich, Dzianis <Dzianis_Haurylovich@epam.com>
2016-03-16 10:19:13 +0300

    Fix the problem with line separators.

--- examples/scripts/post-executed.sh
+++ examples/scripts/post-executed.sh
@@ -7,7 +7,7 @@ VERSION_CODER=coder-7.x-2.5.tar.gz
 CODER=https://ftp.drupal.org/files/projects/$VERSION_CODER
 
 if [ -d "$PHPCS" ]; then
-  echo "`pwd`"
+  echo `pwd`
   echo 'We have to install Drupal code standards'
   cd '/home/vagrant'
   wget "$CODER" | tar -xvf "$VERSION_CODER" | sudo cp -R 'coder/coder_sniffer/Drupal' $PHPCS
